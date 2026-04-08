3f0e703f3e5ef6aa1438fe6143834902038ad8d1
KhoaLND <lengocdangkhoak16@siu.edu.vn>
2026-04-08 10:57:39 +0700

    chore: add cors configuration

--- src/main/java/com/medicology/assessment/config/SecurityConfig.java
+++ src/main/java/com/medicology/assessment/config/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -40,4 +45,27 @@ public class SecurityConfig {
 
         return http.build();
     }
+    @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                
+                // 1. Cho phép các nguồn gửi yêu cầu
+                configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                        "http://localhost:8080"
+                ));
+                
+                // 2. Cho phép các phương thức HTTPS
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
+                
+                // 3. Cho phép tất cả các Header (Authorization, Content-Type, v.v.)
+                configuration.setAllowedHeaders(Arrays.asList("*"));
+                
+                // 4. Cho phép gửi kèm Cookie/Auth Header
+                configuration.setAllowCredentials(true);
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
 }
