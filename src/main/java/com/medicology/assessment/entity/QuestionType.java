3b70200f8c7053e52aad63a956749a3260182f4e
KhoaLND <lengocdangkhoak16@siu.edu.vn>
2026-04-12 02:16:57 +0700

    feat(assessment): add additional question types to QuestionType enum

--- src/main/java/com/medicology/assessment/entity/QuestionType.java
+++ src/main/java/com/medicology/assessment/entity/QuestionType.java
@@ -1,5 +1,10 @@
 package com.medicology.assessment.entity;
 
 public enum QuestionType {
-    SINGLE_CHOICE
+    SINGLE_CHOICE,
+    FILL_IN_THE_BLANKS,
+    SHORT_ANSWER,
+    MATCHING,
+    ORDERING,
+    HOTSPOT_IMAGE
 }
